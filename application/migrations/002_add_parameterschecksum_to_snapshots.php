fc878973778398d82fa5cf4e6620b217ce3fd23f
Roger Qiu <roger.qiu@polycademy.com>
2013-11-11 16:19:15 +1100

    fixed up down migration for parametersChecksum and also added an index to help with select performance

--- application/migrations/002_add_parameterschecksum_to_snapshots.php
+++ application/migrations/002_add_parameterschecksum_to_snapshots.php
@@ -11,12 +11,16 @@ class Migration_add_parameterschecksum_to_snapshots extends CI_Migration {
 			]
 		]);
 
+		$this->db->query('CREATE INDEX idx_parametersChecksum ON snapshots(parametersChecksum)');
+
 	}
 
 	public function down(){
 
-		$this->dbforge->drop_column('snapshots', 'parameters');
+		$this->db->query('DROP INDEX idx_parametersChecksum ON snapshots');
+		
+		$this->dbforge->drop_column('snapshots', 'parametersChecksum');
 	
 	}
-	
+
 }
