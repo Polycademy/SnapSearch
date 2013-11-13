8e1ac169e4ea71832139cdda93916a4d2eabf5ae
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-14 10:44:18 +1100

    models can filter input

--- application/controllers/v1/Robot.php
+++ application/controllers/v1/Robot.php
@@ -15,24 +15,7 @@ class Robot extends CI_Controller{
 
 	public function query(){
 
-		$parameters = array(
-			'url'				=> $this->request->query->get('url'),
-			'width'				=> $this->request->query->get('width'),
-			'height'			=> $this->request->query->get('height'),
-			'imgformat'			=> $this->request->query->get('imgformat'),
-			'useragent'			=> $this->request->query->get('useragent'),
-			'screenshot'		=> $this->request->query->get('screenshot'),
-			'loadimages'		=> $this->request->query->get('loadimages'),
-			'javascriptenabled'	=> $this->request->query->get('javascriptenabled'),
-			'maxtimeout'		=> $this->request->query->get('maxtimeout'),
-			'initialwait'		=> $this->request->query->get('initialwait'),
-			'callback'			=> $this->request->query->get('callback'),
-			'cache'				=> $this->request->query->get('cache'),
-			'cachetime'			=> $this->request->query->get('cachetime'),
-		);
-
-		//array filter to remove the null elements
-		$parameters = array_filter($parameters);
+		$parameters = $this->request->query->all();
 
 		$query = $this->Robot_model->read_site($parameters);
 
@@ -55,7 +38,7 @@ class Robot extends CI_Controller{
 				$content = current($errors);
 			}
 		
-			$code = key($this->Robot_model->get_errors());
+			$code = key($errors);
 			
 			if($code == 'validation_error' OR $code == 'error'){
 				$this->output->set_status_header(400);
@@ -76,24 +59,7 @@ class Robot extends CI_Controller{
 
 	public function post(){
 
-		$parameters = array(
-			'url'				=> $this->request->request->get('url'),
-			'width'				=> $this->request->request->get('width'),
-			'height'			=> $this->request->request->get('height'),
-			'imgformat'			=> $this->request->request->get('imgformat'),
-			'useragent'			=> $this->request->request->get('useragent'),
-			'screenshot'		=> $this->request->request->get('screenshot'),
-			'loadimages'		=> $this->request->request->get('loadimages'),
-			'javascriptenabled'	=> $this->request->request->get('javascriptenabled'),
-			'maxtimeout'		=> $this->request->request->get('maxtimeout'),
-			'initialwait'		=> $this->request->request->get('initialwait'),
-			'callback'			=> $this->request->request->get('callback'),
-			'cache'				=> $this->request->request->get('cache'),
-			'cachetime'			=> $this->request->request->get('cachetime'),
-		);
-
-		//array filter to remove the null elements
-		$parameters = array_filter($parameters);
+		$parameters = $this->request->request->all();
 
 		$query = $this->Robot_model->read_site($parameters);
 		
