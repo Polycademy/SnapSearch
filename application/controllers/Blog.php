3279b70fad263b8c7daff110aa10ee9c6206b649
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-15 12:50:24 +1100

    added blog resource

---
+++ application/controllers/Blog.php
@@ -0,0 +1,38 @@
+<?php
+
+class Blog extends CI_Controller{
+	
+	public function __construct(){
+
+		parent::__construct();
+
+		$this->load->model('Blog_model');
+
+	}
+
+	public function index(){
+
+
+	}
+
+	public function show(){
+
+
+	}
+
+	public function create(){
+
+
+	}
+
+	public function update(){
+
+
+	}
+
+	public function delete(){
+
+
+	}
+
+}
---
+++ application/models/Blog_model.php
@@ -0,0 +1,32 @@
+<?php
+
+class Blog_model extends CI_Controller{
+
+
+	public function __construct(){
+
+		parent::__construct();
+
+	}
+
+	public function create($input_data){
+		
+	}
+
+	public function read($id){
+		
+	}
+
+	public function read_all(){
+		
+	}
+
+	public function update($id, $data){
+		
+	}
+
+	public function delete($id){
+
+	}
+
+}
