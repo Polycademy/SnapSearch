3a174a846589ef58a945f7485dccc0859493a8b6
Roger Qiu <roger.qiu@polycademy.com>
2014-03-14 14:18:43 +1100

    made project work with linux vm enviroment

--- mobilise.dev.sh
+++ mobilise.dev.sh
@@ -7,9 +7,9 @@
 
 # Ask for sudo
 if [[ $UID != 0 ]]; then
-	echo "Please run this script with sudo:"
-	echo "sudo $0 $*"
-	exit 1
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
 fi
 
 # Checking for necessary global components
@@ -31,35 +31,27 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Change to the project's directory
 cd $PROJECT_DIR
 
-# Need something to run change index.php's environment to production
-# perl -pi -e "s/((?:[a-z][a-z]+)\(\'ENVIRONMENT\', isset\(\$_SERVER\[\'CI_ENV\'\]\) \? \$_SERVER\[\'CI_ENV\'\] : \'development\'\).)/define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');/g" index.php
-# Perhaps it's better to use NGINX or the server module to provide this?
-# Eventually mobilise.sh needs to run grunt in order to actually build the final product
-
 # Install all the dependencies
 # Composer and NPM should already be available on PATH
 # Bower and Grunt-Cli will be required however
 # Their STDIN is redirected to /dev/null, this is because it can affect the STDIN of this bash script
 echo "Installing dependencies from Composer, NPM and Bower"
-composer install --no-dev </dev/null
-npm install --production </dev/null
+composer install </dev/null
+npm install </dev/null
 # this has an issue it might request for confirmation (make sure to resolve bower beforehand)
-bower install --production --allow-root </dev/null
-
-# Optimising Composer
-composer dump-autoload --optimize </dev/null
+bower install --allow-root </dev/null
 
 # Download SlimerJS
 read -p "$(tput bold)$(tput setaf 2)Setup SlimerJS? [Y/n]: $(tput sgr0)" -n 1 -r DOWNLOAD_SLIMERJS
 echo
 if [[ $DOWNLOAD_SLIMERJS =~ ^[Y]$ ]]; then
-	echo "Downloading SlimerJS 0.9.0"
-	curl http://download.slimerjs.org/v0.9/0.9.0/slimerjs-0.9.0-linux-i686.tar.bz2 -o slimerjs.tar.bz2
-	echo "Uncompressing and extracting SlimerJS into ./slimerjs"
-	mkdir -p slimerjs && tar xjvf slimerjs.tar.bz2 -C slimerjs --strip-components 1
-	rm slimerjs.tar.bz2
-	echo "Adding SlimerJS to the PATH"
-	sudo ln -sf `pwd`/slimerjs/slimerjs /usr/local/bin/slimerjs
+    echo "Downloading SlimerJS 0.9.0"
+    curl http://download.slimerjs.org/v0.9/0.9.0/slimerjs-0.9.0-linux-i686.tar.bz2 -o slimerjs.tar.bz2
+    echo "Uncompressing and extracting SlimerJS into ./slimerjs"
+    mkdir -p slimerjs && tar xjvf slimerjs.tar.bz2 -C slimerjs --strip-components 1
+    rm slimerjs.tar.bz2
+    echo "Adding slimerjs to the PATH"
+    sudo ln -sf `pwd`/slimerjs/slimerjs /usr/local/bin/slimerjs
 fi
 
 # Switch on globbing extension. It cannot be part of an If block
@@ -69,24 +61,20 @@ shopt -s extglob
 read -p "$(tput bold)$(tput setaf 2)Setup Secrets? [Y/n]: $(tput sgr0)" -n 1 -r DOWNLOAD_SECRETS
 echo
 if [[ $DOWNLOAD_SECRETS =~ ^[Y]$ ]]; then
-	echo "Downloading secret keys relevant to Snapsearch"
-	mkdir -p secrets
-	curl -L -k -u 'CMCDragonkai' https://github.com/CMCDragonkai/keys/tarball/master | tar xzv -C secrets --strip-components 1
-	rm -r secrets/!(snapsearch)
-	cp -r secrets/snapsearch/* secrets
-	rm -r secrets/snapsearch
+    echo "Downloading secret keys relevant to Snapsearch"
+    mkdir -p secrets
+    curl -L -k -u 'CMCDragonkai' https://github.com/CMCDragonkai/keys/tarball/master | tar xzv -C secrets --strip-components 1
+    rm -r secrets/!(snapsearch)
+    cp -r secrets/snapsearch/* secrets
+    rm -r secrets/snapsearch
 fi
-
-# How many robots do you want to start?
-# Prompt for robot numbers, note that port starts at 8500
-# Along with port numbers
 
 # Should create the database if it's not available
 echo "Creating database for snapsearch"
 read -p "$(tput bold)$(tput setaf 2)Enter username for mysql to setup database, followed by enter, or just hit enter to ignore: $(tput sgr0)" -r MYSQL_USER
 # Check if the username has been set
 if [ ! -z "$MYSQL_USER" ]; then
-	mysql -u $MYSQL_USER -p -e "CREATE DATABASE IF NOT EXISTS snapsearch; show databases;"
+    mysql -u $MYSQL_USER -p -e "CREATE DATABASE IF NOT EXISTS snapsearch; show databases;"
 fi
 
 # Migrate all tables
@@ -94,52 +82,35 @@ echo "Migrating the database relies on a proper configuration of the database in
 read -p "$(tput bold)$(tput setaf 2)Migrate the database to latest?. [Y/n]: $(tput sgr0)" -n 1 -r DATABASE_MIGRATION
 echo
 if [[ $DATABASE_MIGRATION =~ ^[Y]$ ]]; then
-	php index.php cli migrate latest
+    php index.php cli migrate latest
 fi
-
-# Setup hosts redirection for snapsearch.io and www.snapsearch.io
-# No longer useful since in production you don't do this, and in development, the VM lets the host machine determine the server name.
-# read -p "$(tput bold)$(tput setaf 2)Setup /etc/hosts redirection for snapsearch.io? [Y/n]: $(tput sgr0)" -n 1 -r HOST_REDIRECTION
-# echo
-# if [[ $HOST_REDIRECTION =~ ^[Y]$ ]]; then
-# 	# This will not overwrite the previous clone
-# 	git clone https://github.com/Polycademy/add-etc-hosts startup_scripts/add-etc-hosts
-# 	echo "Backing up /etc/hosts to startup_scripts/add-etc-hosts/hosts.backup in case of screwup!"
-# 	cp /etc/hosts startup_scripts/add-etc-hosts/hosts.backup
-# 	echo "Adding snapsearch.io and www.snapsearch.io to /etc/hosts"
-# 	sudo startup_scripts/add-etc-hosts/add-etc-hosts snapsearch.io
-# 	sudo startup_scripts/add-etc-hosts/add-etc-hosts www.snapsearch.io
-# fi
 
 # Setting up supervisor upstart script to run this project's robots
 echo "Setting up Supervisor Upstart Script"
 ROBOT_PATH="`pwd`/robot_scripts"
 ESCAPED_ROBOT_PATH="${ROBOT_PATH//\//\\/}"
-perl -pi -e "s/chdir .*/chdir $ESCAPED_ROBOT_PATH/g" startup_scripts/supervisord.conf
-echo "Moving Supervisor startup script to /etc/init"
+echo "Copying Supervisor startup script to /etc/init"
 sudo cp startup_scripts/supervisord.conf /etc/init/supervisord.conf
+echo "Confirming robot script path in the Supervisor startup script"
+sudo perl -pi -e "s/chdir .*/chdir $ESCAPED_ROBOT_PATH/g" /etc/init/supervisord.conf
 echo "Starting Supervisord"
 sudo service supervisord restart
 
 # Setting up NGINX server configuration
 echo "Setting up NGINX configuration"
 ESCAPED_PROJECT_DIR="${PROJECT_DIR//\//\\/}"
-perl -pi -e "s/root .*/root $ESCAPED_PROJECT_DIR;/g" server_config/snapsearch.io
-echo "Establishing a symlink from snapsearch.io to NGINX sites-enabled"
-sudo ln -sf `pwd`/server_config/snapsearch.io /etc/nginx/sites-enabled/snapsearch.io
-echo "Establishing a symlink from SSL certificate and key to NGINX ssl directory"
-mkdir -p /etc/nginx/ssl
-sudo ln -sf `pwd`/secrets/snapsearch.io.pem /etc/nginx/ssl/snapsearch.io.pem
-sudo ln -sf `pwd`/secrets/snapsearch.io.key /etc/nginx/ssl/snapsearch.io.key
-sudo service nginx restart
+echo "Copying snapsearch.io site config to NGINX sites-enabled"
+sudo cp server_config/snapsearch.io /etc/nginx/sites-enabled/snapsearch.io
+echo "Confirming root path in snapsearch.io site config"
+sudo perl -pi -e "s/root .*/root $ESCAPED_PROJECT_DIR;/g" /etc/nginx/sites-enabled/snapsearch.io
+echo "Copying SSL certificate and key to NGINX ssl directory"
+sudo mkdir -p /etc/nginx/ssl
+sudo cp secrets/snapsearch.io.pem /etc/nginx/ssl/snapsearch.io.pem
+sudo cp secrets/snapsearch.io.key /etc/nginx/ssl/snapsearch.io.key
+sudo cp secrets/dev.snapsearch.io.crt /etc/nginx/ssl/dev.snapsearch.io.crt
+sudo cp secrets/dev.snapsearch.io.key /etc/nginx/ssl/dev.snapsearch.io.key
 
-# Setting up Cron Billing
-echo "Setting up SnapSearch billing as a crontab"
-ESCAPED_PROJECT_DIR="${PROJECT_DIR//\//\\/}"
-perl -pi -e "s/PROJECT_DIR/$ESCAPED_PROJECT_DIR/g" startup_scripts/snapsearch
-echo "Copying startup_scripts/snapsearch to /etc/cron.d/snapsearch"
-sudo cp `pwd`/startup_scripts/snapsearch /etc/cron.d/snapsearch
-sudo service cron restart
+sudo service nginx restart
 
 # Changing owner to www-data
 echo "Changing owner of downloaded files to www-data"
--- server_config/snapsearch.io
+++ server_config/snapsearch.io
@@ -9,7 +9,25 @@ upstream robot_slimer {
   
 }
 
-# convert www to non-www redirect 
+# localhost:8499 will load balance between all the robot_slimer servers
+# make sure to firewall 8499 and only allow local requests
+server {
+  
+  listen 8499;
+
+  # No need for gzip compression, this is to be reused by PHP
+  gzip off;
+
+  # No need for tracking access to the load balancer
+  access_log off;
+
+  location / {
+    proxy_pass http://robot_slimer;
+  }
+
+}
+
+# Convert www to non-www redirect 
 server {
 
   listen 80;
@@ -30,7 +48,7 @@ server {
 
 }
 
-# php application
+# Production Environment
 server {
 
   listen 80 deferred;
@@ -38,7 +56,7 @@ server {
   listen 443 deferred ssl;
   listen [::]:443 deferred ssl;
 
-  # The host name to respond to, this will require mapping hostname to ip address on dev server
+  # The host name to respond to
   server_name snapsearch.io;
 
   # Path for static files
@@ -49,7 +67,7 @@ server {
 
   # Force SSL
   if ($ssl_protocol = "") {
-    return 301 https://snapsearch.io$request_uri;
+    return 301 https://$server_name$request_uri;
   }
 
   # SSL Settings
@@ -65,20 +83,9 @@ server {
   include conf.d/cache-busting.conf;
   include conf.d/x-ua-compatible.conf;
   include conf.d/protect-system-files.conf;
+  include conf.d/cache-file-descriptors.conf;
   include conf.d/cross-domain-fonts.conf;
   include conf.d/cross-domain-ajax.conf;
-  # Uncomment this to prevent mobile network providers from modifying your site 
-  # include conf.d/no-transform.conf;
-
-  #Only load cache file descriptors in production
-  if ($server_name !~ "^dev\..+") {
-    include conf.d/cache-file-descriptors.conf;
-  }
-
-  # Force ssl
-  # if ($ssl_protocol = "") {
-  #   return 301 https://snapsearch.io$request_uri;
-  # }
 
   # Removes the initial index or index.php
   # Changes example.com/index.php to example.com/
@@ -108,12 +115,6 @@ server {
     rewrite ^/(.*)$ /index.php?/$1 last;
   }
 
-  # Deny requests to the snapshots cache directory
-  location /snapshots/ {
-    deny all;
-    return 403;
-  }
-
   # Fallback on front controller pattern if it cannot find files or directories matching the uri
   location / {
     try_files $uri $uri/ /index.php;
@@ -131,20 +132,87 @@ server {
 
 }
 
-# localhost:8499 will load balance between all the robot_slimer servers
-# make sure to firewall 8499 and only allow local requests
+# Development environment
 server {
-  
-  listen 8499;
 
-  # No need for gzip compression, this is to be reused by PHP
-  gzip off;
+  listen 80;
+  listen [::]:80;
+  listen 443 ssl;
+  listen [::]:443 ssl;
 
-  # No need for tracking access to the load balancer
-  access_log off;
+  # The host name to respond to, this will require mapping hostname to ip address on dev server
+  server_name dev.snapsearch.io;
 
-  location / {
-    proxy_pass http://robot_slimer;
+  # Path for static files
+  root /www/SnapSearch;
+
+  # Index search file to serve if in a directory
+  index index.php index.html index.htm;
+
+  # Force SSL, supporting tunnelled ports
+  if ($ssl_protocol = "") {
+    return 301 https://$http_host$request_uri;
   }
 
+  # SSL Settings
+  keepalive_timeout 70;
+  ssl_certificate      ssl/dev.snapsearch.io.crt;
+  ssl_certificate_key  ssl/dev.snapsearch.io.key;
+
+  #Specify a charset
+  charset utf-8;
+
+  # Include the recommended base config
+  include conf.d/expires.conf;
+  include conf.d/cache-busting.conf;
+  include conf.d/x-ua-compatible.conf;
+  include conf.d/protect-system-files.conf;
+  # Cache file descriptors has to be disabled in development to allow for immediate changes
+  # include conf.d/cache-file-descriptors.conf;
+  include conf.d/cross-domain-fonts.conf;
+  include conf.d/cross-domain-ajax.conf;
+
+  # Removes the initial index or index.php
+  # Changes example.com/index.php to example.com/
+  # Changes example.com/index to example.com/
+  if ($request_uri ~* ^(/index(.php)?)/?$) {
+    rewrite ^(.*)$ / permanent;
+  }
+
+  # We need a rule to redirect the index.php back to / even when it's between example.com/index.php/controller, so it can go have 301 example.com/controller
+
+  # Removes the index method of every controller
+  # Changes example.com/controller/index to example.com/lol
+  # Changes example.com/controller/index/ to example.com/lol
+  if ($request_uri ~* index/?$) {
+    rewrite ^/(.*)/index/?$ /$1 permanent;
+  }
+
+  # Removes any trailing slashes from uris that are not directories
+  # Changes example.com/controller/ to example.com/controller
+  # Thus normalising the uris
+  if (!-d $request_filename) {
+    rewrite ^/(.+)/$ /$1 permanent;
+  }
+
+  # Send all requests that are not going to a file, directory or symlink to front controllers
+  if (!-e $request_filename) {
+    rewrite ^/(.*)$ /index.php?/$1 last;
+  }
+
+  # Fallback on front controller pattern if it cannot find files or directories matching the uri
+  location / {
+    try_files $uri $uri/ /index.php;
+  }
+
+  # Fast cgi to the PHP run time
+  location ~* \.php$ {
+    try_files $uri =404;
+    include fastcgi_params;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    fastcgi_intercept_errors on;
+    fastcgi_hide_header x-powered-by;
+  }
+  
 }
