abfe6dd694c25ceacc2abcd5ba475d7f6d85338d
Roger Qiu <roger.qiu@polycademy.com>
2014-04-07 03:02:22 +1000

    cleaned up anchor.js

--- js/app/directives/anchor.js
+++ js/app/directives/anchor.js
@@ -5,75 +5,75 @@ var imagesloaded = require('imagesloaded');
 /**
  * Asynchronous Anchor Scroll
  *
- * @param {string} anchor      ID to scroll to
- * @param {string} anchorDelay Delay in microseconds when scrolling
- * @param {string} anchorEvent Event to listen to before scrolling
+ * @param {string}  anchor      ID to scroll to
+ * @param {integer} anchorDelay Delay in microseconds when scrolling
+ * @param {string}  anchorEvent Event to listen to before scrolling
  */
 module.exports = ['$location', '$anchorScroll', '$timeout', function ($location, $anchorScroll, $timeout) {
 
-        return {
-            link: function(scope, element, attributes){
+    return {
+        link: function(scope, element, attributes){
 
-                var id = attributes.anchor || attributes.id || attributes.name;
-                var delay = attributes.anchorDelay;
-                var eventName = attributes.anchorEvent;
-                var firstTimeScrolling = true;
+            var id = attributes.anchor || attributes.id || attributes.name;
+            var delay = attributes.anchorDelay;
+            var eventName = attributes.anchorEvent;
+            var firstTimeScrolling = true;
 
-                element.attr('id', id);
+            element.attr('id', id);
 
-                var scrollToHash = function(hash){
+            var scrollToHash = function(hash){
 
-                    if(id && hash && id === hash){
+                if(id && hash && id === hash){
 
-                        if(delay && firstTimeScrolling){
+                    if(delay && firstTimeScrolling){
 
-                            $timeout(function () {
-
-                                imagesloaded(element, function () {
-                                    $anchorScroll();
-                                });
-
-                            }, delay);
-
-                        }else{
+                        $timeout(function () {
 
                             imagesloaded(element, function () {
                                 $anchorScroll();
                             });
 
-                        }
-                        
-                        //only run the delay the first time this scrolling function executes
-                        //if the hash didn't match, then this function didn't execute!
-                        firstTimeScrolling = false;
+                        }, delay);
+
+                    }else{
+
+                        imagesloaded(element, function () {
+                            $anchorScroll();
+                        });
 
                     }
-
-                };
-                
-                //listen for a custom event, useful if you're waiting on something else to be fully loaded as well
-                if(eventName){
-
-                    scope.$on(eventName, function(){
-
-                        scrollToHash($location.hash());
-
-                    });
+                    
+                    //only run the delay the first time this scrolling function executes
+                    //if the hash didn't match, then this function didn't execute!
+                    firstTimeScrolling = false;
 
                 }
 
-                //hash may be asynchronously changed, the directive may load before the hash is added
-                scope.$watch(function(){
+            };
+            
+            //listen for a custom event, useful if you're waiting on something else to be fully loaded as well
+            if(eventName){
 
-                    return $location.hash();
-                
-                }, function(hash){
+                scope.$on(eventName, function(){
 
-                    scrollToHash(hash);
+                    scrollToHash($location.hash());
 
                 });
 
             }
-        };
+
+            //hash may be asynchronously changed, the directive may load before the hash is added
+            scope.$watch(function(){
+
+                return $location.hash();
+            
+            }, function(hash){
+
+                scrollToHash(hash);
+
+            });
+
+        }
+    };
 
 }];
