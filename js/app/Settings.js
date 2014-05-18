9276977d1b54c1116eed22fd641ee1694f3fd2a4
Roger Qiu <roger.qiu@polycademy.com>
2014-05-19 02:27:32 +1000

    changing to kato chat

--- js/app/Settings.js
+++ js/app/Settings.js
@@ -5,7 +5,7 @@ module.exports = {
         email: 'enquiry@snapsearch.io',
         price: 0.002,
         freeUsageCap: 1000,
-        chatUrl: 'http://www.hipchat.com/gz6yae0iP'
+        chatUrl: 'https://app.kato.im/public#/8f0340e01b870162707d5cba254e10acfc965b3a76be55d567a9c83c8e8631d'
     },
     apiKeys: {}
 };
