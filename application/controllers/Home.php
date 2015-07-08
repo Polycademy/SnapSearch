f6c8c8c0e9aa7ca3cccc3fc0e35551922d12227f
Roger Qiu <roger.qiu@polycademy.com>
2015-07-08 21:44:52 +1000

    no need for output caching on home page, since it is loading a static page with not much dynamism

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -48,11 +48,6 @@ class Home extends CI_Controller{
 		}else{
 
 			$this->auth_response->sendHeaders();
-
-			//when we're in production we can cache the main page for 48 hrs, this requires the cache to be writable, or else this won't work!
-			if(ENVIRONMENT == 'production'){
-				$this->output->cache(2880);
-			}
 			
 			//due to single page app, we're just going with a default layout, no need for server side templating libraries
 			$this->load->view('layouts/default_layout');
